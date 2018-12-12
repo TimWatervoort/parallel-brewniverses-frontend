595f88e377062080009110f7dcebb3c330a361a5
Tim Watervoort <twwatervoort@gmail.com>
2018-12-12 14:43:25 -0700

    switched to hashrouter

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import Navbar from './components/Navbar'
 import SingleChannel from './components/SingleChannel/SingleChannel'
 import Post from './components/Post/Post'
 import AddPostForm from './components/AddPostForm'
-import { Route, Redirect } from 'react-router-dom'
+import { Route, Redirect, Switch } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
+
         <Route exact path = '/' render={() => <Redirect to='/all'/>}/>
-        <Route exact path = '/all' component ={AllChannel} />
+
+        <Route path = '/all' component ={AllChannel} />
         <Route path = '/brewniverse/:channel' component={SingleChannel}/>
         <Route path = '/post/:id' component={Post}/>
         <Route path = '/addpost' component={AddPostForm}/>
