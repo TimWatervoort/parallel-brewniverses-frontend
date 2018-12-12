cf0666d2ee896ae702885ba1af95231ac35e4d95
Tim Watervoort <twwatervoort@gmail.com>
2018-12-12 12:51:03 -0700

    made the headers better

--- src/components/AllChannel/AllChannelHead.js
+++ src/components/AllChannel/AllChannelHead.js
@@ -3,10 +3,15 @@ import React, { Component } from 'react';
 class AllChannelHead extends Component {
   render() {
     return(
-      <div className='container galaxy-purple my-3 py-3 rounded'>
-        <div className='row'>
+      <div className='container galaxy-purple my-3 rounded'>
+        <div className='row pt-3'>
           <div className='col'>
             <h3 className='text-center'>All Brewniverses</h3>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col'>
+            <p className='text-center'>Click on tags to visit Brewniverses!</p>
           </div>
         </div>
       </div>
