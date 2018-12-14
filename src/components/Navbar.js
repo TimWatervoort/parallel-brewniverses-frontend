928feab15907ea63306578b31255cec01c348180
Jean Paul Cornejo <jeanpaulcornejo@gmail.com>
2018-12-14 15:38:47 -0700

    oopsie

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,9 +12,9 @@ class Navbar extends Component {
           <img src={require('./Icon/pb_icon.svg')} height="40" className="d-inline-block align-top ml-1" alt="" />
           <img src={require('./Icon/pb_type.svg')} height="40" className="d-inline-block align-top mx-3" alt="" />
         </Link>
-        <Link className="btn mb-1 nav-link" to={Cookies.get('name') ? '/home' : '/all'}>{Cookies.get('name') ? 'My Brewniverses' : 'All Brewniverses'}</Link>
-        <Link className="btn mb-1 nav-link" to={Cookies.get('name') ? '/addpost' : '/signup'}>{Cookies.get('name') ? 'New Review' : 'Sign Up'}</Link>
-        <Link className="btn mb-1 nav-link" to={Cookies.get('name') ? '/user' : '/login'}>{Cookies.get('name') ? 'My Profile' : 'Log In'}</Link>
+        <Link className="btn mb-1 nav-link" to={Cookies.get('user_id') ? '/home' : '/all'}>{Cookies.get('user_id') ? 'My Brewniverses' : 'All Brewniverses'}</Link>
+        <Link className="btn mb-1 nav-link" to={Cookies.get('user_id') ? '/addpost' : '/signup'}>{Cookies.get('user_id') ? 'New Review' : 'Sign Up'}</Link>
+        <Link className="btn mb-1 nav-link" to={Cookies.get('user_id') ? '/user' : '/login'}>{Cookies.get('user_id') ? 'My Profile' : 'Log In'}</Link>
       </nav>
     )
   }
