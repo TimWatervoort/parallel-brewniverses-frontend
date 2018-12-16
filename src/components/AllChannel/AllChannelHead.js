bf4fcee8336eaeb8ab547b1b15aac77fc74b5292
Jean Paul Cornejo <jeanpaulcornejo@gmail.com>
2018-12-15 19:13:02 -0700

    changes to logo and body background

--- src/components/AllChannel/AllChannelHead.js
+++ src/components/AllChannel/AllChannelHead.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 class AllChannelHead extends Component {
   render() {
     return(
-      <div className='container galaxy-purple my-3 rounded'>
+      <div className='container-fluid header-bg '>
         <div className='row pt-3'>
           <div className='col'>
-            <h3 className='brew-header text-center'>All Brewniverses</h3>
+            <h3 className='brew-header text-center header-text'>All <span className="fas fa-satellite text-white"></span> Brewniverses</h3>
           </div>
         </div>
         <div className='row'>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,8 +9,8 @@ class Navbar extends Component {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <Link className="navbar-brand" to="/all">
-          <img src={require('./Icon/pb_icon.svg')} height="40" className="d-inline-block align-top ml-1" alt="" />
-          <img src={require('./Icon/pb_type.svg')} height="40" className="d-inline-block align-top mx-3" alt="" />
+          <img src={require('./Icon/pb_logo_alt.svg')} height="40" className="d-inline-block align-top ml-1" alt="" />
+          {/* <img src={require('./Icon/pb_type.svg')} height="40" className="d-inline-block align-top mx-3" alt="" /> */}
         </Link>
         <Link className="btn mb-1 nav-link" to={Cookies.get('user_id') ? '/home' : '/all'}>{Cookies.get('user_id') ? 'My Brewniverses' : 'All Brewniverses'}</Link>
         <Link className="btn mb-1 nav-link" to={Cookies.get('user_id') ? '/addpost' : '/signup'}>{Cookies.get('user_id') ? 'New Review' : 'Sign Up'}</Link>
